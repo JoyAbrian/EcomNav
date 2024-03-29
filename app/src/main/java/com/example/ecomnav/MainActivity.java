255ec0e7e5f98f91c8053e42743288a8929029b7
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-30 02:13:48 +0800

    navigation item function

--- app/src/main/java/com/example/ecomnav/MainActivity.java
+++ app/src/main/java/com/example/ecomnav/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.ecomnav;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ImageButton buttonDrawerToggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +24,42 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawerLayout);
         buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
+        navigationView = findViewById(R.id.navigationView);
 
         buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navProfile) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navHome) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navCart) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navFavourite) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navOnGoing) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navHistory) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navFeedback) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navTnc) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navFeedback) {
+                    Toast.makeText(MainActivity.this, itemId, Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.close();
+                
+                return false;
             }
         });
     }
