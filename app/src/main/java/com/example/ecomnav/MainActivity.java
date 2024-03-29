134e11ee7814d7ec890345edefd3422fa6e071da
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-29 23:34:24 +0800

    drawer toggle function

--- app/src/main/java/com/example/ecomnav/MainActivity.java
+++ app/src/main/java/com/example/ecomnav/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.ecomnav;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
+
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
     }
 }
