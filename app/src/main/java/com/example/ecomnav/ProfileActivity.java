916df44ac83bce1a55b8ff6c05b371c69cf901a7
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-30 14:27:34 +0800

    custom edit text

--- app/src/main/java/com/example/ecomnav/ProfileActivity.java
+++ app/src/main/java/com/example/ecomnav/ProfileActivity.java
@@ -1,14 +1,73 @@
 package com.example.ecomnav;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerToggle;
+    NavigationView navigationView;
+    MenuItem currentSceneItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
+        navigationView = findViewById(R.id.navigationView);
+        currentSceneItem = navigationView.getMenu().findItem(R.id.navProfile);
+
+        currentSceneItem.setChecked(true);
+        currentSceneItem.getIcon().setColorFilter(ContextCompat.getColor(this, android.R.color.darker_gray), PorterDuff.Mode.SRC_IN);
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navProfile) {
+                    Toast.makeText(ProfileActivity.this, "profile", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navHome) {
+                    Toast.makeText(ProfileActivity.this, "home", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navCart) {
+                    Toast.makeText(ProfileActivity.this, "cart", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navFavourite) {
+                    Toast.makeText(ProfileActivity.this, "favourite", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navOnGoing) {
+                    Toast.makeText(ProfileActivity.this, "onGoing", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navHistory) {
+                    Toast.makeText(ProfileActivity.this, "history", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navFeedback) {
+                    Toast.makeText(ProfileActivity.this, "feedback", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navTnc) {
+                    Toast.makeText(ProfileActivity.this, "terms and conditions", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.navContactUs) {
+                    Toast.makeText(ProfileActivity.this, "contact us", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.close();
+
+                return false;
+            }
+        });
+
     }
 }
